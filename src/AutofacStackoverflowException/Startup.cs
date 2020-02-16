75fc0b104113b798eecda8c6b2b759cfc00d65ce
Admir Tuzovic <admir.tuzovic@appimpact.ba>
2020-02-16 12:06:51 +0100

    Removed unused namespaces.

--- src/AutofacStackoverflowException/Startup.cs
+++ src/AutofacStackoverflowException/Startup.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Autofac;
 using AutofacStackoverflowException.IoC;
 using Microsoft.AspNetCore.Builder;
@@ -10,7 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AutofacStackoverflowException
 {
